b7ac8003e9b8c0e286de7d63d63757cc54d5955a
jkaushal947 <47825941+jkaushal947@users.noreply.github.com>
2021-03-18 17:56:58 +0000

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import logo from '../logo.svg';
+import logo from '../jk_phone_store_logo.png';
 import styled from 'styled-components';
 import {ButtonContainer} from './Button';
 
@@ -8,17 +8,11 @@ export default class Navbar extends Component {
     render() {
         return (
             <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-                {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
-                Creative Commons (Attribution 3.0 Unported);
-                https://www.iconfinder.com/Makoto_msk */}
             <Link to="/">
             <img src={logo} alt="store" className="navbar-brand"/>
             </Link>
             <ul className="navbar-nav align-items-center">
             <li className="nav-item ml-5">
-            <Link to="/" className="nav-link">
-            Products
-            </Link>
             </li>
             </ul>
             <Link to="/cart" className="ml-auto">
